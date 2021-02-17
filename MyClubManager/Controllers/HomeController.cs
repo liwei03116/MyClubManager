690daaad5edc990b76cef10514db3c997f620ef3
liwei03116 <liwei03116@hotmail.com>
2021-02-17 17:35:49 +1100

    Updated Information page and home page

--- MyClubManager/Controllers/HomeController.cs
+++ MyClubManager/Controllers/HomeController.cs
@@ -26,5 +26,6 @@ namespace MyClubManager.Controllers
 
             return View();
         }
+
     }
 }
---
+++ MyClubManager/Controllers/InformationController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyClubManager.Controllers
+{
+    public class InformationController : Controller
+    {
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
